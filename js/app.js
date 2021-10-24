0661d59af848afa1e8042db515954b0368e3e8bf
aibreich <aibreich@iu.edu>
2021-10-24 14:21:25 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
+function route() {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#/", "");
+
+    if (pageID == "") {
+        model.placholder("home")
+    } else {
+        model.placholder(pageID)
+    }
+}
+
+function initListeners(){
+    $(window).on("hashchange", route);
+    route();
+}
+
+$(document).ready(function(){
+    initListeners();
+    model.placholder("home")
+}) 
+
 // this is my function to set up each of the var
 const navMove = () => {
     const burger = document.querySelector(".fas")
---
+++ js/model.js
@@ -0,0 +1,12 @@
+var model = (function() {
+    var _placholder = function (pageID) {
+        $.get(`pages/${pageID}/${pageID}.html`, function(data){
+            $("#app").html(data)
+        });
+    
+        
+    };
+    return {
+        placholder: _placholder
+    };
+    })();
